2fde5b5b1c52780becfa39516b3cf351f9067939
Awesometimei <118484408+Awesometimei@users.noreply.github.com>
2025-03-06 18:04:35 +0000

    DANNY

--- Main.java
+++ Main.java
@@ -10,11 +10,30 @@ public class Main
 
   public static int sumOfDiag(int[][] matrix)
   {
-    return 0;
+    int sum = 0;
+    for (int i = 0; i < matrix.length; i++)
+    {
+      for (int j = 0; j < matrix[i].length;j++)
+      {
+        if (i==j)
+        {
+          sum += matrix[i][j];
+        }
+      }
+    }
+    return sum;
   }
 
   public static int[][] productTable(final int numRows, final int numCols)
   {
-    return null;
+    int[][] multTable = new int[numCols][numRows];
+    for (int i = 0; i < numsCols; i++)
+    {
+      for (int j = 0; j < numRows; j++)
+      {
+        multTable[i][j] = i*j;
+      }
+    }
+    return multTable;
   }
 }
